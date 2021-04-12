3b67b486f900462cd41ef6ec6f1efcef2f155787
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-04-12 10:10:36 +0200

    10:10

--- K_Eszter_Nezoter_2021.04.12/Program.cs
+++ K_Eszter_Nezoter_2021.04.12/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace K_Eszter_Nezoter_2021._04._12
 {
     class Program
     {
+        struct Adat
+        {
+            public string foglaltsag;
+            public string kategoria;
+        }
         static void Main(string[] args)
         {
+            //1.feladat
+            //Struktúra példányosítása
+            Adat[] adatok  = new Adat[15];
+
+            //Beolvasás
+            StreamReader olvas = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2014-oktober_uj\foglaltsag.txt");
+            int n  =  0;
+            while (!olvas.EndOfStream)
+            {
+                string sor  =  olvas.ReadLine();
+                adatok[n].foglaltsag  = sor;
+                n++;
+            }
+            olvas.Close();
+            
+            StreamReader olvas2 = new StreamReader(@"E:\OneDrive - Kisvárdai SZC Móricz Zsigmond Szakgimnáziuma és Szakközépiskolája\Oktatas\Programozas\Jakab_Acs_Eszter\Erettsegi_feladatok\2014-oktober_uj\kategoria.txt");
+            n = 0;
+            while (!olvas2.EndOfStream)
+            {
+                string sor = olvas2.ReadLine();
+                adatok[n].kategoria = sor;
+                n++;
+            }
+            olvas2.Close();
+            Console.WriteLine("1.feladat\nBeolvasás kész!");
+
+            //2.feladat
+            Console.ReadKey();
         }
     }
 }
